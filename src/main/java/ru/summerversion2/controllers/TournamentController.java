db559c82d6f6ed4ad11beb6b1fb256abe509d975
dashushka <wallabyDo@mail.ru>
2023-07-06 23:55:16 +0400

    add controllers

---
+++ src/main/java/ru/summerversion2/controllers/TournamentController.java
@@ -0,0 +1,9 @@
+package ru.summerversion2.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class TournamentController {
+}
